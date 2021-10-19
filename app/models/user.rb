1b18b049f50ba9c5fef853d7f202340efcf5b3ed
saitonaoki <reisa.naoki@icloud.com>
2021-10-19 15:41:03 +0900

    follor機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    def follow!(other_user)
+                      active_relationships.create!(followed_id: other_user.id)
+                    end            
+                    def following?(other_user)
+                      active_relationships.find_by(followed_id: other_user.id)
+                    end
                     
-end
+                    def unfollow!(other_user)
+                      active_relationships.find_by(followed_id: other_user.id).destroy
+                  
+                    end
+                    end
